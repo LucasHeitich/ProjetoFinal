20b7fd8a3b9507ee5c6d3426aba601df9ce09117
Leandro zilli <leandro_zilli_@hotmail.com>
2013-09-03 11:22:46 -0300

    ALterado a o pacote repository

--- ProjetoFinal/src/com/betha/beans/CadastroUserBean.java
+++ ProjetoFinal/src/com/betha/beans/CadastroUserBean.java
@@ -10,7 +10,7 @@ import com.betha.util.Repositorios;
 
 /**
  * @author Administrador
- *LUCAS
+ * leandro zilli 
  */
 public class CadastroUserBean {
 	
@@ -35,7 +35,7 @@ public class CadastroUserBean {
 		UserDao ud = new UserDao();
 		
 		//ud.insert(this.user);
-		Repositorios.getUsuarios().insert(this.user);
+		
 		
 		//System.out.println(this.user.getHobbies().get(1));
 		this.cadastro=true;
---
+++ ProjetoFinal/src/com/betha/repository/EnderecoRepo.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.betha.repository;
+
+import java.util.List;
+
+import com.betha.cadastro.Endereco;
+
+/**
+ * @author Leandro zilli
+ *
+ */
+public interface EnderecoRepo {
+
+	public List<Endereco> listar();
+	
+	public List<Endereco> orderAsc();
+	
+	public List<Endereco> orderDesc();
+	
+	public Object porCodigo();
+	
+}
